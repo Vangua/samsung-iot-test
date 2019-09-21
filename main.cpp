75c11fb4ed07581d33b27464c7bc4fe232048312
Ван Андрей Гуанович <van_ag@tomsk.lemz.ru>
2019-09-21 13:41:31 +0700

    добавлен обход в ширину

--- main.cpp
+++ main.cpp
@@ -1,26 +1,60 @@
-// g++ (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0
+// Used compilier: g++ (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0
 
 #include <iostream>
+#include <vector>
 
 #define SIZE 5
 
 using std::cout;
 using std::endl;
+using std::vector;
 
+bool contains(const vector<int>& vec, int val)
+{
+    for (const auto& item : vec)
+        if (item == val)
+            return true;
+    return false;
+}
+
+void bf(bool matrix[SIZE][SIZE], int vertex)
+{
+    vector<int> used(1, vertex);
+    vector<int> queue;
+
+    for (int i = 0; i < SIZE; ++i)
+        if (matrix[vertex][i])
+            queue.push_back(i);
+
+    while (queue.size())
+    {
+        vertex = queue.front();
+        queue.erase(queue.begin());
+        
+        if ( contains(used, vertex) )
+            continue;
+        
+        for (int i = 0; i < SIZE; ++i)
+            if (matrix[vertex][i] and !contains(used, vertex))
+                queue.push_back(i);
+        
+        used.push_back(vertex);
+    }
+ 
+    for (auto item : used)
+        cout << item << " ";
+    cout << endl;
+
+}
 
 int main()
 {
-    int vertex_matrix [SIZE][SIZE] =  { 0, 1, 1, 0, 0,
-                                        1, 0, 0, 0, 0,
+    bool vertex_matrix [SIZE][SIZE] = { 0, 1, 1, 1, 1,
                                         1, 0, 0, 0, 1,
-                                        0, 0, 0, 0, 1,
-                                        0, 0, 1, 1, 0 };
-    
-    for (int i = 0; i < SIZE; ++i)
-    {
-        for (int j = 0; j < SIZE; ++j)
-            cout << vertex_matrix[i][j] << '\t';
-        cout << endl;
-    }
+                                        1, 0, 0, 1, 0,
+                                        1, 0, 1, 0, 0,
+                                        1, 1, 0, 0, 0 };
+    bf(vertex_matrix, 1);
     return 0;
 }
+
